f7674359e639330dd37fc59dcc1f253a37f46d3d
José González Silva <gonzalez17831@iesmarenostrum.com>
2018-10-05 18:15:36 +0200

    - Tratando de darle caña a la clase mundial

--- Mundial_Futbol/mundial.cs
+++ Mundial_Futbol/mundial.cs
@@ -3,18 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace Mundial_Futbol
 {
     class Mundial
     {
-        private List<Partido> partidos;
-        private int anyo;
-
         public Mundial()
         {
-
+            //Partidos = "";
+            Anyo = new DateTime().Year;
         }
 
+        public void Cargar(int anyo)
+        {
+            
+           
+        }
+
+        /*public Jugador BuscarJugador(string nombre)
+        {
+
+            return Jugador;
+        } */
+
+        public List<Partido> Partidos { get; set; }
+
+        public int Anyo { get; set; }
     }
 }
